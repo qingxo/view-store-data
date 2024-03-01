81db164f75fa498ce8393b41c1f9f081b722a62f
qingxo <qingxo@sina.com>
2024-03-01 16:46:47 +0800

    add 拖动组件

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from 'antd';
 import DragItem from './components/dragItem';
-
+import './App.css';
 const App = () => (
   <div className="App">
     <Button type="primary">Button</Button>
--- src/components/dragItem/DragItem.jsx
+++ src/components/dragItem/DragItem.jsx
@@ -1,6 +1,42 @@
 import React from "react";
+import { Button } from 'antd';
+import Draggable from 'react-draggable';
+import { MenuUnfoldOutlined } from '@ant-design/icons';
 
 export default function DragItem() {
-    return <div>xxx drag item</div>
+    const eventLogger = (e, data) => {
+        console.log('Event: ', e);
+        console.log('Data: ', data);
+    };
+
+    const handleStart = (e, data) => {
+        console.log("the handleStart :", e, data);
+    }
+
+    const handleDrag = (e, data) => {
+        console.log("the handleDrag :", e, data);
+
+    }
+    const handleStop = (e, data) => {
+        console.log("the handleStop :", e, data);
+
+    }
+    return (
+        <Draggable
+            axis="both"
+            handle=".handle"
+            defaultPosition={{ x: 0, y: 0 }}
+            position={null}
+            grid={[25, 25]}
+            scale={1}
+            onStart={handleStart}
+            onDrag={handleDrag}
+            onStop={handleStop}>
+            <div>
+                <div className="handle"><Button type="primary"><MenuUnfoldOutlined /></Button></div>
+                <div>This readme is really dragging on...</div>
+            </div>
+        </Draggable>
+    );
 }
 
