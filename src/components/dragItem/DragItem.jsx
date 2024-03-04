1d89a468e814cd4491b516db185385d534d829fa
qingxo <qingxo@sina.com>
2024-03-04 13:59:30 +0800

    update

--- src/components/dragItem/DragItem.jsx
+++ src/components/dragItem/DragItem.jsx
@@ -2,7 +2,20 @@ import React, { useState } from "react";
 import { Button } from 'antd';
 import Draggable from 'react-draggable';
 import { MenuUnfoldOutlined } from '@ant-design/icons';
-
+import JSONViewer from 'react-json-viewer';
+import './index.less';
+const jsons = {
+    "abc": {
+        a: 'xxxx',
+        b: 'bbbb',
+        d: 'dddd'
+    },
+    "xyz": '123',
+    "adbc": [
+        'abc', 'fff', '1234', 4243
+    ],
+    "asdf": 4
+}
 export default function DragItem() {
     const [isShow, setIsShow] = useState(false);
     const eventLogger = (e, data) => {
@@ -26,6 +39,39 @@ export default function DragItem() {
     const showDetail = () => {
         setIsShow(!isShow);
     }
+
+    const getSessionInfo = () => {
+        let target = {};
+        for (let key in sessionStorage) {
+            if (key !== 'length' && !(typeof sessionStorage[key] === 'function')) {
+                target[key] = sessionStorage[key];
+            }
+        }
+        return target;
+    }
+
+    const getLocalhostInfo = () => {
+        let target = {};
+        for (let key in localStorage) {
+            if (key !== 'length' && !(typeof localStorage[key] === 'function')) {
+                target[key] = localStorage[key];
+            }
+        }
+        return target;
+    }
+
+    const getCookieInfo = () => {
+        let target = {};
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim(); // 去除首尾空格
+            var cookieParts = cookie.split('=');
+            var key = cookieParts[0];
+            var value = cookieParts[1];
+            target[key] = value;
+        }
+        return target;
+    }
     return (
         <Draggable
             axis="both"
@@ -37,9 +83,27 @@ export default function DragItem() {
             onStart={handleStart}
             onDrag={handleDrag}
             onStop={handleStop}>
-            <div>
-                <div className="handle"><Button onDoubleClick={showDetail} type="primary"><MenuUnfoldOutlined /></Button></div>
-                <div style={{ display: isShow ? 'block' : 'none' }}>This readme is really dragging on...</div>
+            <div className="zoom">
+                <div className="handle">
+                    <Button onDoubleClick={showDetail} type="primary"><MenuUnfoldOutlined /></Button>
+
+                    <div style={{ display: isShow ? 'block' : 'none' }} className="infos">
+                        <div className="title">LocalStorage信息</div>
+                        <JSONViewer
+                            json={getLocalhostInfo()}
+                        />
+                        <div className="title">Session信息</div>
+                        <JSONViewer
+                            json={getSessionInfo()}
+                        />
+
+                        <div className="title">Cookie信息</div>
+                        <JSONViewer
+                            json={getCookieInfo()}
+                        />
+                    </div>
+                </div>
+
             </div>
         </Draggable>
     );
