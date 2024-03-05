422f319a7f551a113984a313716b1cf4a2818c0b
qingxo <qingxo@sina.com>
2024-03-05 10:34:05 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import reportWebVitals from './reportWebVitals';
 import DragItem from './components/dragItem';
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <DragItem />
+  </React.StrictMode>
+);
 
 // // If you want to start measuring performance in your app, pass a function
 // // to log results (for example: reportWebVitals(console.log))
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
 
-module.exports = DragItem;
+// module.exports = DragItem;
