906f436ab44ff222423d25bbb139939fe683e1c0
qingxo <qingxo@sina.com>
2024-03-05 15:09:54 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,9 @@ module.exports = {
                 exclude: /(node_modules|bower_components)/,
                 use: {
                     loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
                 },
                 resolve: {
                     extensions: ['.js', '.jsx'],
