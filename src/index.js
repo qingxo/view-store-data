c524a753eaf4c404d9caae92160872222f889b2d
qingxo <qingxo@sina.com>
2024-03-27 11:35:18 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import DragItem from './components/dragItem';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <DragItem />
-  </React.StrictMode>
-);
 
 export default DragItem;
